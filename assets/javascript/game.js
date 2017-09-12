6c217fbbad79dfc912b63ad29e8d7876dd25c2bd
Evan Lu <evanlu@Evans-MacBook-Pro.local>
2017-09-12 15:50:59 -0700

    else ifs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,11 @@ var losses = 0;
 var total = 0;
 var counter = 0;
 var randomNumber = "";
+var firstNumber = "";
+var secondNumber = "";
+var thirdNumber = "";
+var fourthNumber = "";
+
 
 
 // this generates a random number between 19 and 120 for the user to guess.
@@ -16,39 +21,27 @@ var randomNumber =
 
 // the following are pictures that generate a number 1-12 when clicked.
 $(".clickme").on("click", function() {
-	counter ++;
+    counter++;
     // console.log (counter);
     document.getElementById("clicked").innerHTML = counter;
     var firstNumber =
         Math.floor((Math.random() * 12) + 1);
-     console.log(firstNumber);
-    if (counter===2) {
-        $(".clickme").on("click", function() {
+    console.log(firstNumber);
+    if (counter === 2) {
+        $("#happy").on("click", function() {
             document.getElementById("clicked").innerHTML = counter;
             var secondNumber =
                 Math.floor((Math.random() * 12) + 1);
             var total = secondNumber + firstNumber;
-            console.log (total)
-
+            console.log(total)
         })
+        } else if (counter === 3) {
+        $("#happy").on("click", function() {
+            document.getElementById("clicked").innerHTML = counter;
+            var thirdNumber =
+                Math.floor(Math.random() * 12) + 1;
+            var total = thirdNumber + secondNumber + firstNumber;
+            console.log(total);
+}});
+     
 
-
-    }
-})
-
-
-
-
-//             if (firstNumber === generateNumber) {
-//                 alert("you win");
-//             }; 
-// }
-// else {
-//     $(".clickme").on("click", function() {
-//                 var secondNumber =
-//                     Math.floor((Math.random() * 12) + 1);
-//                 console.log(secondNumber)
-//                 var total = firstNumber + secondNumber;
-//                 console.log(total)
-
-//           } }) };
