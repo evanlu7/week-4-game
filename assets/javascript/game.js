fe88a9a427e8df8191919ffea3a74e589563992e
Evan Lu <evanlu@Evans-MacBook-Pro.local>
2017-09-14 12:51:57 -0700

    added sounds/styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,111 @@
-var wins = 0;
-var losses = 0;
-var total = happyNumber + screamingNumber + faceNumber + woahNumber;
-var counter = 0;
-var randomNumber = "";
-var happyNumber = "";
-var screamingNumber = "";
-var faceNumber = "";
-var woahNumber = "";
+//there is a max of 7 numbers generated.
 
-console.log(total)
-// random saved code
-// counter++;
-// document.getElementById("clicked").innerHTML = counter;
+var myObject = {
+    wins: 0,
+    losses: 0,
+    total: 0,
+    counter: 0,
+    randomNumber: 0,
+    happyNumber: 0,
+    screamingNumber: 0,
+    faceNumber: 0,
+    woahNumber: 0,
+    total: 0,
 
+}
+
+// restart function
+function restart() {
+    var myObject = {
+        wins: 0,
+        losses: 0,
+        total: 0,
+        counter: 0,
+        randomNumber: 0,
+        happyNumber: 0,
+        screamingNumber: 0,
+        faceNumber: 0,
+        woahNumber: 0,
+        total: 0,
+    }
+}
+
+
+//update total function
+function updateTotal() {
+ myObject.total= myObject.happyNumber + myObject.screamingNumber + myObject.faceNumber + myObject.woahNumber
+}
+//outputs total score
+var totalScore =
+    document.getElementById("totalpoints").innerHTML = myObject.total
 // this generates a random number between 19 and 120 for the user to guess.
 var generateNumber = Math.floor((Math.random() * 120) + 19);
-// console.log(generateNumber);
+console.log(generateNumber);
 
 // this displays the number for the user to see.
 var randomNumber =
     document.getElementById("numberToGuess").innerHTML = generateNumber;
+
+// outputs wins
+var wins =
+    document.getElementById("wins").innerHTML = myObject.wins;
+
+//outputs losses
+var losses =
+    document.getElementById("losses").innerHTML = myObject.losses
+
+
+
+function play() {
+    var audio = document.getElementById("audio");
+}
 
 
 // the following are pictures that generate a number 1-12 when clicked.
 $("#happy").on("click", function generateHappyNumber() {
     var happyNumber =
         Math.floor(Math.random() * 12) + 1;
-    console.log(happyNumber);
+    // console.log(happyNumber);
+    audio.play();
+    updateTotal();
+    console.log (myObject.total)
 })
 
 
 $("#screaming").on("click", function generateScreamingNumber() {
     var screamingNumber =
         Math.floor(Math.random() * 12) + 1;
-    console.log(screamingNumber);
+    // console.log(screamingNumber);
+    audio.play();
+    updateTotal();
+
+
 })
 
 $("#face").on("click", function generateFaceNumber() {
     var faceNumber =
         Math.floor(Math.random() * 12) + 1;
-    console.log(faceNumber)
+    // console.log(faceNumber);
+    audio.play();
+    updateTotal();
 })
 
 $("#woah").on("click", function generateWoahNumber() {
     var woahNumber =
         Math.floor(Math.random() * 12) + 1;
-    console.log(woahNumber)
+    // console.log(woahNumber);
+    audio.play();
+    updateTotal();
 })
 
+
+// if else for wins / losses.  updates counter, calls restart function.
+if (myObject.updateTotal === myObject.randomNumber) {
+    alert("You Win!")
+    wins++;
+    restart()
+} else if (myObject.updateTotal > myObject.randomNumber) {
+    losses++;
+    alert("You Lose!")
+    restart()
+}
