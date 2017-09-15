e0bf54203f371a46468fa1f60c82f0836328e40b
Evan Lu <evanlu@Evans-MacBook-Pro.local>
2017-09-15 15:48:11 -0700

    console log works, few minor errors

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,24 +3,20 @@
 var myObject = {
     wins: 0,
     losses: 0,
-    total: 0,
-    counter: 0,
-    randomNumber: 0,
     happyNumber: Math.floor(Math.random() * 12 + 1),
-    screamingNumber: 0,
-    faceNumber: 0,
-    woahNumber: 0
+    screamingNumber: Math.floor(Math.random() * 12 +1),
+    faceNumber: Math.floor(Math.random() * 12 +1),
+    woahNumber: Math.floor(Math.random() * 12 +1),
+    total: 0
 }
-// var happyNumber = ;
-// var screamingNumber = ;
-// var faceNumber = ;
-// var woahNumber = ;
 
 
-//outputs total score
+
+
 var totalScore =
-    document.getElementById("totalpoints").innerHTML = 0 + this.total
+    document.getElementById("totalpoints").innerHTML = total;
 // this generates a random number between 19 and 120 for the user to guess.
+
 var generateNumber = Math.floor((Math.random() * 120) + 19);
 console.log(generateNumber);
 
@@ -34,48 +30,106 @@ var wins =
 
 //outputs losses
 var losses =
-    document.getElementById("losses").innerHTML = myObject.losses
+    document.getElementById("losses").innerHTML = myObject.losses;
+
+var total = 0;
 
 
+function updateTotalHappy () {
+    total += myObject.happyNumber;
+    console.log (total);
+
+    if (total === generateNumber ) {
+    alert("You Win!")
+    wins++;
+    restart()
+} else if (total > generateNumber) {
+    losses++;
+    alert("You Lose!")
+    restart()
+}
+}
+
+function updateTotalScreaming () {
+    total += myObject.screamingNumber;
+    console.log (total);
+
+      if (total === generateNumber ) {
+    alert("You Win!")
+    wins++;
+    restart()
+} else if (total > generateNumber) {
+    losses++;
+    alert("You Lose!")
+    restart()
+}
+}
+
+function updateTotalFace () {
+    total += myObject.faceNumber;
+    console.log (total);
+      if (total === generateNumber ) {
+    alert("You Win!")
+    wins++;
+    restart()
+} else if (total > generateNumber) {
+    losses++;
+    alert("You Lose!")
+    restart()
+}
+
+}
+
+function updateTotalWoah () {
+    total += myObject.woahNumber;
+    console.log (total);
+      if (total === generateNumber ) {
+    alert("You Win!")
+    wins++;
+    restart()
+} else if (total > generateNumber) {
+    losses++;
+    alert("You Lose!")
+    restart()
+}
+}
 // the following are pictures that generate a number 1-12 when clicked.
-
 $("#happy").on("click", function generateHappyNumber() {
     audio.play();
     console.log(myObject.happyNumber);
+    updateTotalHappy();
+
 })
 
 
 $("#screaming").on("click", function generateScreamingNumber() {
-    var screamingNumber =
-        Math.floor(Math.random() * 12) + 1;
-    console.log(screamingNumber);
     audio.play();
+      console.log(myObject.screamingNumber);
+      updateTotalScreaming();
+    
 })
 
 $("#face").on("click", function generateFaceNumber() {
-    var faceNumber =
-        Math.floor(Math.random() * 12) + 1;
-    // console.log(faceNumber);
-    audio.play();
-    updateTotal();
+     audio.play();
+      console.log(myObject.faceNumber);
+        updateTotalFace();
 })
 
 $("#woah").on("click", function generateWoahNumber() {
-    var woahNumber =
-        Math.floor(Math.random() * 12) + 1;
-    // console.log(woahNumber);
-    audio.play();
-    updateTotal();
+     audio.play();
+      console.log(myObject.woahNumber);
+      updateTotalWoah ();
+   
 })
 
 
 
 // if else for wins / losses.  updates counter, calls restart function.
-if (myObject.updateTotal === myObject.randomNumber) {
+if (total === generateNumber ) {
     alert("You Win!")
     wins++;
     restart()
-} else if (myObject.updateTotal > myObject.randomNumber) {
+} else if (total > generateNumber) {
     losses++;
     alert("You Lose!")
     restart()
