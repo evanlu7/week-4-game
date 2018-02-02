940f2f16abc73beb1553175987420265c2749c3f
Evan Lu <evanlu7@gmail.com>
2018-02-01 21:37:56 -0800

    fixed the adding function, help from keith

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,101 +1,90 @@
-//there is a max of 7 numbers generated.
+var wins = 0;
+var losses = 0;
+var addition =0;
+var total = Math.floor((Math.random() * 100) + 19);
+var happyNumber = Math.floor(Math.random() * 12) + 1;
+var screamingNumber = Math.floor(Math.random() * 12) + 1;
+var faceNumber = Math.floor(Math.random() * 12) + 1;
+var woahNumber = Math.floor(Math.random() * 12) + 1;
+var addition = 0;
+ 
 
-var myObject = {
-    wins: 0,
-    losses: 0,
-    total: 0,
-    counter: 0,
-    randomNumber: 0,
-    happyNumber: Math.floor(Math.random() * 12 + 1),
-    screamingNumber: 0,
-    faceNumber: 0,
-    woahNumber: 0
+
+
+var updateAdd = function () {
+        $('.addition').empty();
+        $('.addition').append(addition);
+
+        $('#wins').empty();
+        $('#wins').append(wins);
+
+        $('#losses').empty();
+        $('#losses').append(losses);
+    }
+
+function setScore(){
+    document.getElementById("totalpoints").innerHTML = addition;
 }
-// var happyNumber = ;
-// var screamingNumber = ;
-// var faceNumber = ;
-// var woahNumber = ;
 
-
-//outputs total score
-var totalScore =
-    document.getElementById("totalpoints").innerHTML = 0 + this.total
-// this generates a random number between 19 and 120 for the user to guess.
 var generateNumber = Math.floor((Math.random() * 120) + 19);
-console.log(generateNumber);
-
-// this displays the number for the user to see.
-var randomNumber =
-    document.getElementById("numberToGuess").innerHTML = generateNumber;
-
-// outputs wins
-var wins =
-    document.getElementById("wins").innerHTML = myObject.wins;
-
-//outputs losses
-var losses =
-    document.getElementById("losses").innerHTML = myObject.losses
+document.getElementById("randomNumber").innerHTML = generateNumber;
 
 
-// the following are pictures that generate a number 1-12 when clicked.
 
 $("#happy").on("click", function generateHappyNumber() {
     audio.play();
-    console.log(myObject.happyNumber);
+    addition = addition + happyNumber;
+    setScore()
+    game();
+
 })
-
-
 $("#screaming").on("click", function generateScreamingNumber() {
-    var screamingNumber =
-        Math.floor(Math.random() * 12) + 1;
-    console.log(screamingNumber);
     audio.play();
-})
+    addition = addition + screamingNumber;
+    setScore()
+    game();
 
+})
 $("#face").on("click", function generateFaceNumber() {
-    var faceNumber =
-        Math.floor(Math.random() * 12) + 1;
-    // console.log(faceNumber);
     audio.play();
-    updateTotal();
+    addition = addition + faceNumber;
+    setScore()
+    game();
+
 })
 
 $("#woah").on("click", function generateWoahNumber() {
-    var woahNumber =
-        Math.floor(Math.random() * 12) + 1;
-    // console.log(woahNumber);
     audio.play();
-    updateTotal();
+    addition = addition + woahNumber;
+    setScore()
+    game();
 })
 
 
-
-// if else for wins / losses.  updates counter, calls restart function.
-if (myObject.updateTotal === myObject.randomNumber) {
-    alert("You Win!")
-    wins++;
-    restart()
-} else if (myObject.updateTotal > myObject.randomNumber) {
-    losses++;
-    alert("You Lose!")
-    restart()
+function game() {
+    // if else for wins / losses.  updates counter, calls restart function.
+    if (addition === total) {
+        alert("You Win!")
+        wins++;
+        restart()
+    } else if (addition > total) {
+        losses++;
+        alert("You Lose!")
+        restart()
+    } else updateAdd();
 }
+
+
 // restart function
 function restart() {
-    var myObject = {
-        wins: 0,
-        losses: 0,
-        total: 0,
-        counter: 0,
-        randomNumber: 0,
-        happyNumber: 0,
-        screamingNumber: 0,
-        faceNumber: 0,
-        woahNumber: 0,
-        total: 0,
+    addition = 0;
+    total = Math.floor((Math.random() * 100) + 19);
+    var happyNumber= Math.floor(Math.random() * 12) + 1;
+    var screamingNumber= Math.floor(Math.random() * 12) + 1;
+    var faceNumber= Math.floor(Math.random() * 12) + 1;
+    var woahNumber= Math.floor(Math.random() * 12) + 1;
+    
+    function play() {
+        var audio = document.getElementById("audio");
     }
-// audio click
-function play() {
-    var audio = document.getElementById("audio");
-}
 }
